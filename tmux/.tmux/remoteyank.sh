8bc65caa6cbfd3f145f14476f9ac0af90cedc190
Leo310 <leodev310@gmail.com>
2022-01-23 20:36:53 +0100

    Add first steps for my own working remote clipboard

--- tmux/.tmux/remoteyank.sh
+++ tmux/.tmux/remoteyank.sh
@@ -2,12 +2,8 @@
 
 set -eu
 
-# is_app_installed() {
-#   type "$1" &>/dev/null
-# }
-
 # get data either form stdin or from file. "$@" returns array of every argument
 buf=$(cat "$@")
 
 # some nc need -c flag to close instantly after connection 
-echo $buf | xclip -sel clip -i
+echo $buf | nc -q0 localhost 5556
