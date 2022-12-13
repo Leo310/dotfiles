5b0812f41e352210c8118fc7180df07218ef7a71
Leo310 <leodev310@gmail.com>
2022-12-14 00:00:27 +0100

    Update: improved lualine

--- nvim/.config/nvim/lua/leo310/lualine.lua
+++ nvim/.config/nvim/lua/leo310/lualine.lua
@@ -8,10 +8,7 @@ require('lualine').setup {
 		theme = 'auto',
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
-		disabled_filetypes = {
-			statusline = {},
-			winbar = {},
-		},
+		disabled_filetypes = {},
 		ignore_focus = {},
 		always_divide_middle = true,
 		globalstatus = false,
@@ -40,5 +37,5 @@ require('lualine').setup {
 	tabline = {},
 	winbar = {},
 	inactive_winbar = {},
-	extensions = {}
+	extensions = { 'nvim-tree', 'fugitive' }
 }
