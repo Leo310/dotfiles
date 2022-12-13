8d149ef2cc07ea3d37944fcc82d1ff46437960f3
Leo310 <leodev310@gmail.com>
2022-12-14 00:01:02 +0100

    Update: removed lualine from packer window

--- nvim/.config/nvim/lua/leo310/lualine.lua
+++ nvim/.config/nvim/lua/leo310/lualine.lua
@@ -8,7 +8,7 @@ require('lualine').setup {
 		theme = 'auto',
 		component_separators = { left = '', right = '' },
 		section_separators = { left = '', right = '' },
-		disabled_filetypes = {},
+		disabled_filetypes = { 'packer' },
 		ignore_focus = {},
 		always_divide_middle = true,
 		globalstatus = false,
