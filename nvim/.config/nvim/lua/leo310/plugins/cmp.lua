52099d4552afddedeb8b23283d2b3e8e3c230886
Leo310 <leodev310@gmail.com>
2023-01-12 16:05:00 +0100

    Add: path completion

--- nvim/.config/nvim/lua/leo310/plugins/cmp.lua
+++ nvim/.config/nvim/lua/leo310/plugins/cmp.lua
@@ -1,122 +1,123 @@
 return {
-	'hrsh7th/nvim-cmp',
-	event = "InsertEnter",
-	dependencies = {
-		'hrsh7th/cmp-nvim-lsp',
-		'hrsh7th/cmp-buffer',
-		'onsails/lspkind.nvim', -- icons of functions. can also define own icons (lookup nvim-cmp wiki -> cuztomize appearance)
-		'hrsh7th/cmp-cmdline',
-		'hrsh7th/cmp-path',
-		-- Vsnip
-		'hrsh7th/vim-vsnip',
-		'hrsh7th/cmp-vsnip',
-		'rafamadriz/friendly-snippets',
-	},
-	config = function()
-		-- Setup nvim-cmp.
-		local cmp = require 'cmp'
-		local lspkind = require 'lspkind'
-		local function fillSpacesToFixed(str, untilLength)
-			local untilFixed = untilLength - string.len(str)
-			local postfix = ''
-			for i = 1, untilFixed, 1 do postfix = postfix .. ' ' end
-			return str .. postfix
-		end
+    'hrsh7th/nvim-cmp',
+    event = "InsertEnter",
+    dependencies = {
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-buffer',
+        'onsails/lspkind.nvim', -- icons of functions. can also define own icons (lookup nvim-cmp wiki -> cuztomize appearance)
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-path',
+        -- Vsnip
+        'hrsh7th/vim-vsnip',
+        'hrsh7th/cmp-vsnip',
+        'rafamadriz/friendly-snippets',
+    },
+    config = function()
+        -- Setup nvim-cmp.
+        local cmp = require 'cmp'
+        local lspkind = require 'lspkind'
+        local function fillSpacesToFixed(str, untilLength)
+            local untilFixed = untilLength - string.len(str)
+            local postfix = ''
+            for i = 1, untilFixed, 1 do postfix = postfix .. ' ' end
+            return str .. postfix
+        end
 
-		vim.opt.completeopt = "menu,menuone,noselect"
+        vim.opt.completeopt = "menu,menuone,noselect"
 
-		cmp.setup({
-			snippet = {
-				expand = function(args)
-					-- For `vsnip` user.
-					vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
+        cmp.setup({
+            snippet = {
+                expand = function(args)
+                    -- For `vsnip` user.
+                    vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` user.
 
-					-- For `luasnip` user.
-					-- require('luasnip').lsp_expand(args.body)
+                    -- For `luasnip` user.
+                    -- require('luasnip').lsp_expand(args.body)
 
-					-- For `ultisnips` user.
-					-- vim.fn["UltiSnips#Anon"](args.body)
-				end,
-			},
-			window = {
-				completion = {
-					border = {
-						'╭', '─', '╮', '│', '╯', '─', '╰', '│'
-					},
-					winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
-					-- scrollbar = '║'
-				},
-				documentation = {
-					winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
-					-- scrollbar = '',
-					border = {
-						'─',
-						'─',
-						'╮',
-						'│',
-						'╯',
-						'─',
-						'─',
-						''
-					}
-				},
-			},
-			formatting = {
-				fields = { 'kind', 'abbr', 'menu' },
-				format = function(entry, vim_item)
-					vim_item.menu = ({
-						nvim_lsp = "ﲳ",
-						nvim_lua = "",
-						treesitter = "",
-						path = "ﱮ",
-						buffer = "B﬘",
-						copilot = "",
-						zsh = "",
-						vsnip = "",
-						spell = "暈",
-					})[entry.source.name]
-					if vim_item.menu ~= nil then
-						-- buffers with code mostly
-						vim_item.menu = fillSpacesToFixed(vim_item.kind, 10) .. vim_item.menu
-					end
-					vim_item.kind = lspkind.symbolic(vim_item.kind, { with_text = false })
-					return vim_item
-				end,
-			},
-			view = {
-				entries = { name = 'custom', selection_order = 'near_cursor' }
-			},
-			mapping = {
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<Tab>'] = cmp.mapping.select_next_item(),
-				['<S-Tab>'] = cmp.mapping.select_prev_item(),
-				['<C-Space>'] = cmp.mapping.complete({}),
-				['<C-e>'] = cmp.mapping.close(),
-				['<CR>'] = cmp.mapping.confirm({ select = true }),
-			},
-			sources = {
-				{ name = "copilot" },
-				{ name = 'nvim_lsp' },
-				{ name = 'vsnip' },
-				{ name = 'buffer' },
-			}
-		})
+                    -- For `ultisnips` user.
+                    -- vim.fn["UltiSnips#Anon"](args.body)
+                end,
+            },
+            window = {
+                completion = {
+                    border = {
+                        '╭', '─', '╮', '│', '╯', '─', '╰', '│'
+                    },
+                    winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
+                    -- scrollbar = '║'
+                },
+                documentation = {
+                    winhighlight = 'Normal:CmpPmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None',
+                    -- scrollbar = '',
+                    border = {
+                        '─',
+                        '─',
+                        '╮',
+                        '│',
+                        '╯',
+                        '─',
+                        '─',
+                        ''
+                    }
+                },
+            },
+            formatting = {
+                fields = { 'kind', 'abbr', 'menu' },
+                format = function(entry, vim_item)
+                    vim_item.menu = ({
+                        nvim_lsp = "ﲳ",
+                        nvim_lua = "",
+                        treesitter = "",
+                        path = "ﱮ",
+                        buffer = "B﬘",
+                        copilot = "",
+                        zsh = "",
+                        vsnip = "",
+                        spell = "暈",
+                    })[entry.source.name]
+                    if vim_item.menu ~= nil then
+                        -- buffers with code mostly
+                        vim_item.menu = fillSpacesToFixed(vim_item.kind, 10) .. vim_item.menu
+                    end
+                    vim_item.kind = lspkind.symbolic(vim_item.kind, { with_text = false })
+                    return vim_item
+                end,
+            },
+            view = {
+                entries = { name = 'custom', selection_order = 'near_cursor' }
+            },
+            mapping = {
+                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<Tab>'] = cmp.mapping.select_next_item(),
+                ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                ['<C-Space>'] = cmp.mapping.complete({}),
+                ['<C-e>'] = cmp.mapping.close(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            },
+            sources = {
+                { name = 'path' },
+                { name = "copilot" },
+                { name = 'nvim_lsp' },
+                { name = 'vsnip' },
+                { name = 'buffer' },
+            }
+        })
 
-		cmp.setup.cmdline('/', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				name = 'buffer'
-			},
-		})
+        cmp.setup.cmdline('/', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                name = 'buffer'
+            },
+        })
 
-		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-		cmp.setup.cmdline(':', {
-			mapping = cmp.mapping.preset.cmdline(),
-			sources = {
-				{ name = 'path' },
-				{ name = 'cmdline' },
-			},
-		})
-	end
+        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = {
+                { name = 'path' },
+                { name = 'cmdline' },
+            },
+        })
+    end
 }
--- nvim/.config/nvim/lua/leo310/plugins/dashboard.lua
+++ nvim/.config/nvim/lua/leo310/plugins/dashboard.lua
@@ -1,43 +1,43 @@
 return {
-	'glepnir/dashboard-nvim',
-	init = function()
-		-- Close NerdTree buffer before auto-saving the current session
-		vim.api.nvim_create_autocmd('User', {
-			pattern = 'DBSessionSavePre',
-			callback = function()
-				vim.cmd('NvimTreeClose')
-			end,
-		})
-	end,
-	config = function()
-		local db = require('dashboard')
-		vim.keymap.set("n", "<leader>dd", ":Dashboard<CR>", { desc = 'Open Dashboard' })
+    'glepnir/dashboard-nvim',
+    init = function()
+        -- Close NerdTree buffer before auto-saving the current session
+        vim.api.nvim_create_autocmd('User', {
+            pattern = 'DBSessionSavePre',
+            callback = function()
+                vim.cmd('NvimTreeClose')
+            end,
+        })
+    end,
+    config = function()
+        local db = require('dashboard')
+        vim.keymap.set("n", "<leader>dd", ":Dashboard<CR>", { desc = 'Open Dashboard' })
 
-		db.session_auto_save_on_exit = true
-		db.session_directory = vim.fn.stdpath('cache') .. "/session"
-		db.session_verbose = true
+        db.session_auto_save_on_exit = true
+        db.session_directory = vim.fn.stdpath('cache') .. "/session"
+        db.session_verbose = true
 
-		db.footer_pad = 4
-		db.custom_footer = { '🪩 To the moon!!! 🚀' }
-		db.preview_file_height = 20
-		db.preview_file_width = 40
-		math.randomseed(os.time())
-		local randomNumber = math.random(4)
-		local speed = '1.0'
-		if randomNumber == 1 then
-			speed = '0.4'
-		elseif randomNumber == 2 then
-			speed = '0.6'
-		end
-		db.preview_file_path = "~/.config/nvim/db/head" .. randomNumber .. ".gif"
-		db.preview_command = 'chafa -c full --fg-only --speed ' .. speed .. ' --symbols braille'
+        db.footer_pad = 4
+        db.custom_footer = { '🪩 To the moon!!! 🚀' }
+        db.preview_file_height = 20
+        db.preview_file_width = 40
+        math.randomseed(os.time())
+        local randomNumber = math.random(4)
+        local speed = '1.0'
+        if randomNumber == 1 then
+            speed = '0.4'
+        elseif randomNumber == 2 then
+            speed = '0.6'
+        end
+        db.preview_file_path = "~/.config/nvim/db/head" .. randomNumber .. ".gif"
+        db.preview_command = 'chafa -c full --fg-only --speed ' .. speed .. ' --symbols braille'
 
-		db.custom_center = {
-			{ icon = '  ', desc = 'Find file              ', action = 'Telescope find_files' },
-			{ icon = '  ', desc = 'Recently latest session', action = 'SessionLoad' },
-			{ icon = '  ', desc = 'My Projects            ', action = '!tmux neww ~/.tmux/sessionizer.sh' },
-			{ icon = '  ', desc = 'New file               ', action = 'DashboardNewFile' },
-			-- { icon = '  ', desc = 'Recently Used Files    ', action = 'Telescope oldfiles' }
-		}
-	end
+        db.custom_center = {
+            { icon = '  ', desc = 'Find file              ', action = 'Telescope find_files' },
+            { icon = '  ', desc = 'Recently latest session', action = 'SessionLoad' },
+            { icon = '  ', desc = 'My Projects            ', action = '!tmux neww ~/.tmux/sessionizer.sh' },
+            { icon = '  ', desc = 'New file               ', action = 'DashboardNewFile' },
+            -- { icon = '  ', desc = 'Recently Used Files    ', action = 'Telescope oldfiles' }
+        }
+    end
 }
