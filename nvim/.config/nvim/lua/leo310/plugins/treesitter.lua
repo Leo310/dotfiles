4ab9136d991a693eb06e7539598605cab04bbd8f
Leo310 <leodev310@gmail.com>
2023-04-11 22:43:58 +0200

    Plugin changes

--- nvim/.config/nvim/lua/leo310/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/leo310/plugins/treesitter.lua
@@ -1,19 +1,18 @@
 return {
-	{ "nvim-treesitter/playground", cmd = "TSPlaygroundToggle" },
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		event = "BufReadPost",
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter-context',
-		},
-		config = function()
-			require 'nvim-treesitter.configs'.setup {
-				highlight = {
-					enable = true,
-				}
-			}
-			require 'treesitter-context'.setup({ enable = true })
-		end
-	}
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = "BufReadPost",
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter-context",
+        },
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                highlight = {
+                    enable = true,
+                },
+            })
+            require("treesitter-context").setup({ enable = true })
+        end,
+    },
 }
--- nvim/.config/nvim/lua/leo310/plugins/utils.lua
+++ nvim/.config/nvim/lua/leo310/plugins/utils.lua
@@ -2,8 +2,6 @@ return {
     "nvim-lua/plenary.nvim",
     "christoomey/vim-tmux-navigator",
     "RyanMillerC/better-vim-tmux-resizer",
-    "jiangmiao/auto-pairs",
     "tpope/vim-commentary",
-    "gpanders/editorconfig.nvim",
     "ThePrimeagen/refactoring.nvim",
 }
