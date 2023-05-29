039573fe878b8d6a3f6989a61fb14cb0691de598
Leo310 <leodev310@gmail.com>
2023-05-29 00:42:16 -0700

    feat(nvimtree): floating file tree + telescope ignore files

--- nvim/.config/nvim/lua/leo310/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/leo310/plugins/nvimtree.lua
@@ -13,6 +13,8 @@ return {
         vim.g.loaded_netrwPlugin = 1
     end,
     config = function()
+        local HEIGHT_RATIO = 0.8
+        local WIDTH_RATIO = 0.5
         require("nvim-tree").setup({
             sort_by = "case_sensitive",
             actions = {
@@ -23,6 +25,34 @@ return {
             git = {
                 enable = true,
                 ignore = false,
+            },
+            view = {
+                relativenumber = true,
+                -- centered floating window
+                float = {
+                    enable = true,
+                    open_win_config = function()
+                        local screen_w = vim.opt.columns:get()
+                        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+                        local window_w = screen_w * WIDTH_RATIO
+                        local window_h = screen_h * HEIGHT_RATIO
+                        local window_w_int = math.floor(window_w)
+                        local window_h_int = math.floor(window_h)
+                        local center_x = (screen_w - window_w) / 2
+                        local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+                        return {
+                            border = "rounded",
+                            relative = "editor",
+                            row = center_y,
+                            col = center_x,
+                            width = window_w_int,
+                            height = window_h_int,
+                        }
+                    end,
+                },
+                width = function()
+                    return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+                end,
             },
         })
     end,
