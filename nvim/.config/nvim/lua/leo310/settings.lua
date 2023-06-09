ed64d8bb18a69a380874c97285256030caa62d1d
Leo310 <leodev310@gmail.com>
2023-06-08 19:37:47 -0700

    feat(settings): remove tilde symbol on empty lines

--- nvim/.config/nvim/lua/leo310/settings.lua
+++ nvim/.config/nvim/lua/leo310/settings.lua
@@ -1,4 +1,4 @@
-vim.opt.mouse = 'a'
+vim.opt.mouse = "a"
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -15,6 +15,8 @@ vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
+
+vim.opt.fillchars = { eob = " " }
 
 vim.opt.termguicolors = true
 
