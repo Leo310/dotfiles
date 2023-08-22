97473a290e49e89962732c0bd206f17c438e6381
Leo310 <leodev310@gmail.com>
2023-08-22 09:16:54 +0200

    feat: new better neogit

--- nvim/.config/nvim/lua/leo310/plugins/telescope.lua
+++ nvim/.config/nvim/lua/leo310/plugins/telescope.lua
@@ -58,6 +58,7 @@ return {
                     "%.glb",
                     "%.svg",
                     "%.webm",
+                    "%.webp",
                 },
             },
             pickers = {
